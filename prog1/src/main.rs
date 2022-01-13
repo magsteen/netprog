0431544320ddb80c81bc619ba985126d2e7f3ade
Magnus Steensland <mag.steensland@gmail.com>
2022-01-13 10:02:11 +0100

    Changed from interval to a step solution.

--- prog1/src/main.rs
+++ prog1/src/main.rs
@@ -1,67 +1,72 @@
 use std::env;
 use std::thread;
-use std::sync::{Arc, Mutex};
-
-//Other strategy: Each threads send found prime numbers to the same channel. 
-//One threads continuesly pulls data from channel and adds it to a min heap.
-//Hepa is sorted when pulled. 
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
 
     if args[1] == "-h" {
-        println!("HELP: run command (where arguments are number inputs): <binary> 'start' 'end 'num_ofthreads'");
+        println!("HELP: run command: <executable> 'start' 'end 'num_of_threads'");
+        return
+    }
+    else if args.len() != 4 {
+        println!("Insufficient number of datapoints provided. Use '-h' for help." );
         return
     }
 
-    let range_start = args[1].parse::<i64>().unwrap();
-    let range_end = args[2].parse::<i64>().unwrap();
-    let num_threads = args[3].parse::<i64>().unwrap();
-
-    println!("Welcom to primenumber finder with extra steps!");
-
-    let mut threads = Vec::new();
-    let mut all_primes = Vec::new();
-    let mut intervals: Vec<i64> = Vec::new();
-    find_intervals(&mut intervals, range_start, range_end, num_threads);
-    println!("Intervals: {:?}", intervals);
-
-    let m = Arc::new(Mutex::new(intervals));
-
-    for i in 0..num_threads {
-        let m_clone = m.clone();
-        threads.push(thread::spawn(move || {
-            let intervals = m_clone.lock().unwrap();
-            let mut start = range_start;
-            for index in 1..i+1 {
-                start = start + intervals[index as usize - 1];
-            }
-            let end = start + intervals[i as usize];
-            drop(intervals);
-            find_primes(start, end)
-        }));
+    //Input verification
+    let range_start;
+    let input1 = args[1].parse::<u32>();
+    match input1 {
+        Ok(input1) => {range_start = input1}
+        Err(_input1) => {println!("Datapoint 1 is not an integer."); return}
+    }
+    let range_end;
+    let input2 = args[2].parse::<u32>();
+    match input2 {
+        Ok(input2) => {range_end = input2}
+        Err(_input2) => {println!("Datapoint 2 is not an integer."); return}
+    }
+    let num_threads;
+    let input3 = args[3].parse::<u32>();
+    match input3 {
+        Ok(input3) => {num_threads = input3}
+        Err(_input3) => {println!("Datapoint 3 is not an integer."); return}
     }
 
-    let mut result_primes: Vec<i64>;
+    //Main program
+    let mut threads = Vec::new();
+    let mut all_primes = Vec::new();
+
+    for i in 0..num_threads {
+        threads.push(thread::spawn(move || {
+            let data = find_thread_input(range_start + i, range_end, num_threads as usize);
+            find_primes(data)
+        }));
+    }
+    let mut result_primes: Vec<u32>;
     for thread in threads {
         result_primes = thread.join().unwrap();
+        println!("Thread res: {:?}", result_primes);
+
         all_primes.append(&mut result_primes);
     }
 
-    println!("Result: {:?}", all_primes);
+    all_primes.sort();
+    println!("Final sorted result: {:?}", all_primes);
 }
 
-fn find_primes(start: i64, end: i64) -> Vec<i64> {
+//Finds and returns the primes in the given dataset.
+fn find_primes(data: Vec<u32>) -> Vec<u32> {
     let mut primes = Vec::new();
-    for num in start..end {
+    for num in data {
         if num < 2 {continue}
         if num == 2 {
             primes.push(num);
             continue;
         }
         let num_f = num as f64;
-        let num_square = num_f.sqrt().ceil() as i64 + 1;
+        let num_square = num_f.sqrt().ceil() as u32 + 1;
         let mut is_prime: bool = true;
         for i in 2..num_square {
             if num % i == 0 {
@@ -71,19 +76,13 @@ fn find_primes(start: i64, end: i64) -> Vec<i64> {
         }
         if is_prime {primes.push(num)}
     }
-    //println!("thread res: {:?}", primes);
 
     primes
 }
 
-fn find_intervals(intervals: &mut Vec<i64>, range_start:i64, range_end:i64, num_threads:i64) {
-    let total_range_length = range_end - range_start;
-    let base_interval_length = total_range_length / num_threads;
-
-    let mut dividable_rest = total_range_length % num_threads as i64;
-    println!("rest: {}", dividable_rest);
-    for _ in 0..num_threads as usize{
-        dividable_rest = dividable_rest / 2;
-        intervals.push(base_interval_length + dividable_rest);
+//Returns all data points between given start and end, with a give stepsize
+fn find_thread_input(start:u32, end:u32, step:usize) -> Vec<u32> {
+    (start..=end)
+        .step_by(step)
+        .collect()
     }
-}
