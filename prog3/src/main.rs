97aefc7a650ab192ee1b9d4260edc24310348306
Magnus Steensland <mag.steensland@gmail.com>
2022-02-13 14:13:25 +0100

    Concurrent math solution

--- prog3/src/main.rs
+++ prog3/src/main.rs
@@ -1,14 +1,11 @@
-use core::panic;
-use std::error::Error;
-use std::f32::consts::E;
-use std::io;
-use std::io::prelude::*;
-use std::net::TcpListener;
-use std::net::TcpStream;
-use std::net::ToSocketAddrs;
-use std::os::unix::thread;
-use std::ptr::NonNull;
-use std::thread::JoinHandle;
+use std::env;
+use std::fmt;
+use std::result;
+
+use async_std::io::prelude::BufReadExt;
+use async_std::io::{BufReader, ReadExt, WriteExt};
+use async_std::prelude::*;
+use async_std::{io, net::TcpListener, net::TcpStream, task};
 
 const MATH_SERVER_ADDRESS: &str = "localhost:8080";
 const HTTP_SERVER_ADDRESS: &str = "localahost:8081";
@@ -20,11 +17,17 @@ struct Expression {
 }
 
 fn main() {
-    // Listen for incoming TCP connections on localhost port 7878
+    let args: Vec<String> = env::args().collect();
+    match args[1].as_str() {
+        "-s" => start_servers(),
+        "-c" => start_math_client(),
+        _ => panic!("You must specify a client/server argument (-c or -s)"),
+    };
+}
+
+fn start_servers() {
     let mut handles = Vec::new();
 
-    handles.push(std::thread::spawn(|| start_math_client()));
-    handles.push(std::thread::spawn(|| start_web_client()));
     handles.push(std::thread::spawn(|| start_math_server()));
     handles.push(std::thread::spawn(|| start_web_server()));
 
@@ -34,139 +37,76 @@ fn main() {
 }
 
 fn start_math_client() {
-    println!("Starting MATH client...");
-    loop {
-        println!("Please provide an expression to calculate (e.g: <number><operator><number>): ");
-        let mut input = String::new();
-        let output: f32;
-        match io::stdin().read_line(&mut input) {
-            Ok(_) => {
-                input.pop().unwrap();
-                let mut stream: TcpStream;
-                match TcpStream::connect(MATH_SERVER_ADDRESS) {
-                    Ok(val) => {
-                        println!("Connected to server: {}", val.peer_addr().unwrap());
-                        stream = val;
-                    }
-                    Err(e) => {
-                        println!("Error: {}", e);
-                        return;
-                    }
-                };
-                stream = send_tcp_request(stream, input);
-                output = recieve_response(stream);
-                println!("Response/Answer: {}", output);
-            }
-            Err(error) => {
-                println!("Error: {}", error)
-            }
-        };
-    }
+    task::block_on(async_math_client()).unwrap();
 }
-
-fn start_web_client() {}
 
 fn start_math_server() {
-    let listener = TcpListener::bind(MATH_SERVER_ADDRESS).unwrap();
-    for stream in listener.incoming() {
-        handle_math_connection(stream.unwrap());
-    }
+    task::block_on(async_math_server()).unwrap();
 }
 
-fn start_web_server() {}
+async fn async_math_client() -> io::Result<()> {
+    println!("Starting MATH client...");
+    let mut stream = TcpStream::connect(MATH_SERVER_ADDRESS).await?;
 
-fn handle_math_connection(mut stream: TcpStream) {
-    let mut buf = [0; 1024];
-    let n = stream.read(&mut buf).unwrap();
-    let byte_data = &buf[..n];
-    let input = match std::str::from_utf8(&byte_data) {
-        Ok(res) => res,
-        Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
-    };
-
-    println!("Got input: {}, with length: {:?}", input, input.len());
-    let expr = translate_to_expression(input.to_string());
-    match expr {
-        Ok(v) => {
-            match calculate(v) {
-                Ok(answer) => {
-                    //write response with result
-                    stream.write(&answer.to_be_bytes()).unwrap();
-                    println!("Answer: {:?}", answer);
-                }
-                Err(e) => println!("Calculation failed: {:?}", e),
-            };
-        }
-        Err(e) => println!("Error: {}", e), //"input couldn 't be translated to an expression: {e}"
-    }
-}
-
-fn handle_web_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 1024];
-    stream.read(&mut buffer).unwrap();
-
-    let get = b"GET / HTTP/1.1\r\n";
-
-    let (status_line, html) = if buffer.starts_with(get) {
-        (
-            "HTTP/1.1 200 OK\r\n\r\n",
-            "<!DOCTYPE html>
-        <html lang='no'>
-          <head>
-            <meta charset='utf-8'>
-            <title>Calcumulator</title>
-          </head>
-          <body>
-            <h1>Hei og velkommen til min web-tjener</h1>
-            <div>Header fra klient gav følgende: "
-                .to_owned()
-                + std::str::from_utf8(&buffer).unwrap()
-                + "
-            </div>
-          </body>
-        </html>
-        ",
-        )
-    } else {
-        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "".to_owned())
-    };
-    //println!("{}", std::str::from_utf8(&buffer).unwrap());
-    let response = format!("{status_line}{html}");
-    stream.write_all(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
-}
-
-fn send_tcp_request(mut stream: TcpStream, input: String) -> TcpStream {
-    match stream.write(input.as_bytes()) {
-        Ok(n) => {
-            println!("Wrote {} bytes", n)
-        }
-        Err(e) => {
-            println!("Error: {}", e);
-        }
-    }
-    return stream;
-}
-
-fn recieve_response(mut stream: TcpStream) -> f32 {
-    let mut buf = [0; 4];
-    let mut counter = 0;
     loop {
-        counter += 1;
-        println!("Count: {}", counter);
-        match stream.read(&mut buf) {
-            Ok(v) => {
-                let n = v;
-                println!("Read {} bytes", n);
-                break;
-            }
-            Err(e) => {
-                println!("Error: {}", e);
-            }
+        let mut input = String::new();
+        let mut output = [0; 4];
+
+        println!("Please provide an expression to calculate (e.g: <number><operator><number>): ");
+        io::stdin().read_line(&mut input).await?;
+
+        stream.write(input.as_bytes()).await?;
+        if stream.read(&mut output).await? == 0 {
+            break;
         };
+        let result = f32::from_be_bytes([output[0], output[1], output[2], output[3]]);
+        println!("Answer: {}", result)
+    }
+    Ok(())
+}
+
+async fn async_math_server() -> io::Result<()> {
+    let listener = TcpListener::bind(MATH_SERVER_ADDRESS).await?;
+    let mut incoming = listener.incoming();
+    while let Some(stream) = incoming.next().await {
+        let stream = stream?;
+        task::spawn(handle_math_connection(stream));
     }
 
-    return f32::from_be_bytes(buf);
+    Ok(())
+}
+
+async fn handle_math_connection(mut stream: TcpStream) -> io::Result<()> {
+    loop {
+        let mut data = [0; 1024];
+        let bytes = stream.read(&mut data).await?;
+        //println!("Receive");
+        let byte_data = &data[..bytes - 1]; //Drop newline char at end of message
+        let input = match std::str::from_utf8(&byte_data) {
+            Ok(res) => res,
+            Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+        };
+
+        println!("Got input: {:?}, with length: {:?}", input, input.len());
+        if input.starts_with("exit") {
+            break;
+        }
+        let expr = translate_to_expression(input.to_string());
+        match expr {
+            Ok(v) => {
+                match calculate(v) {
+                    Ok(answer) => {
+                        //println!("Send");
+                        stream.write(&answer.to_be_bytes()).await?;
+                    }
+                    Err(e) => println!("Server says: Calculation failed: {:?}", e),
+                };
+            }
+            Err(e) => println!("Server says: Error: {}", e), //"input couldn 't be translated to an expression: {e}"
+        }
+    }
+
+    Ok(())
 }
 
 fn translate_to_expression(expr: String) -> Result<Expression, &'static str> {
@@ -212,3 +152,42 @@ fn get_op_index(expr: String) -> Result<usize, &'static str> {
     }
     return Err("Operator not found in expression!"); //panic!("Operator not found in expression!");
 }
+
+fn start_web_client() {}
+
+fn start_web_server() {}
+
+// fn handle_web_connection(mut stream: TcpStream) {
+//     let mut buffer = [0; 1024];
+//     stream.read(&mut buffer).unwrap();
+
+//     let get = b"GET / HTTP/1.1\r\n";
+
+//     let (status_line, html) = if buffer.starts_with(get) {
+//         (
+//             "HTTP/1.1 200 OK\r\n\r\n",
+//             "<!DOCTYPE html>
+//         <html lang='no'>
+//           <head>
+//             <meta charset='utf-8'>
+//             <title>Calcumulator</title>
+//           </head>
+//           <body>
+//             <h1>Hei og velkommen til min web-tjener</h1>
+//             <div>Header fra klient gav følgende: "
+//                 .to_owned()
+//                 + std::str::from_utf8(&buffer).unwrap()
+//                 + "
+//             </div>
+//           </body>
+//         </html>
+//         ",
+//         )
+//     } else {
+//         ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "".to_owned())
+//     };
+//     //println!("{}", std::str::from_utf8(&buffer).unwrap());
+//     let response = format!("{status_line}{html}");
+//     stream.write_all(response.as_bytes()).unwrap();
+//     stream.flush().unwrap();
+// }
